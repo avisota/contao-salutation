f5c858176730fea6a07d618b1cd0cc7c194a18b7
Tristan Lins <tristan.lins@bit3.de>
2014-03-11 15:52:42 +0100

    Add module description.

---
+++ module/languages/en/modules.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-salutation
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Module
+ */
+$GLOBALS['TL_LANG']['MOD']['avisota-salutation'] = array(
+	'Avisota - Salutation',
+	'Manage flexible and dynamic salutations for Avisota messages.'
+);

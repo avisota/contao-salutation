e63dd22a0351ae776e3d4ea02f3616409bcd4b3d
Avibot <avibot@avisota.org>
2014-05-26 04:00:19 +0200

    Auto-Update translations from transifex

---
+++ module/languages/de/orm_avisota_recipient.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-03-25T14:17:41+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['salutation']['0'] = 'Anrede';
+$GLOBALS['TL_LANG']['orm_avisota_recipient']['salutation']['1'] = 'Bitte wählen Sie die bevorzugte Anrede aus.';
+

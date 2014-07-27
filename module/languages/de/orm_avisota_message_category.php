ba65cae714fe6ed121f607b3f6c420255803351a
Avibot <avibot@avisota.org>
2014-07-27 04:01:24 +0200

    Auto-Update translations from transifex

---
+++ module/languages/de/orm_avisota_message_category.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-05-31T04:01:08+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['orm_avisota_message_category']['salutation']['0']   = 'Anrede';
+$GLOBALS['TL_LANG']['orm_avisota_message_category']['salutation']['1']   = 'Bitte wählen Sie die Anrede für Empfänger aus dieser Quelle aus.';
+$GLOBALS['TL_LANG']['orm_avisota_message_category']['salutation_legend'] = 'Automatische Anrede';
+
